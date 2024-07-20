c7da4e73c73cdf80aa37288eefac584eff417995
Ben Hutchings <benh@debian.org>
2024-07-20 05:02:37 +0200

    d/b/buildcheck.py: Restore reading of config to pass to check classes
    This was left broken after switching the config format to TOML.
    
    Fixes: a7a67cc5b947 ("Use new TOML config")

--- debian/bin/buildcheck.py
+++ debian/bin/buildcheck.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
 
+import itertools
+import pathlib
 import sys
+
+from debian_linux.config_v2 import Config
 
 
 class Main(object):
     def __init__(self, dir, arch, featureset, flavour):
         self.args = dir, arch, featureset, flavour
+
+        config_dirs = [
+            pathlib.Path('debian/config'),
+            pathlib.Path('debian/config.local'),
+        ]
+        top_config = Config.read_orig(config_dirs).merged
+        arch_config = next(
+            ac
+            for ac in itertools.chain.from_iterable(
+                kac.debianarchs for kac in top_config.kernelarchs)
+            if ac.name == arch
+        )
+        fs_config = next(fsc for fsc in arch_config.featuresets
+                         if fsc.name == featureset)
+        self.config = next(fc for fc in fs_config.flavours
+                           if fc.name == flavour)
 
     def __call__(self):
         fail = 0
